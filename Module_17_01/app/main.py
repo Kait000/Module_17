9999a3e276ed0bce428831e5efc7145cf3b9f1d5
Роман <x-sel@Rambler.ru>
2025-01-09 22:10:15 +0500

    Add files via upload

---
+++ Module_17_01/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routers import user, task
+
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
+
+
+@app.get('/')
+async def welcome() -> dict:
+    return {"message": "Welcome to Taskmanager"}
+
+app.include_router(user.router)
+app.include_router(task.router)
